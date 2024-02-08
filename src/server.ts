cc66db4987afa8590e21a8cd77b719e87e9dba08
Carlos R Orioli <rogeriorioli@gmail.com>
2024-02-07 23:22:28 -0300

    ajuste deploy

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express, { Express } from "express";
 import {route} from "./routes";
 import cors from 'cors'
 import path from "path";
+import 'dotenv/config'
 const app: Express = express();
 const tmpFolder = path.join(__dirname, "../","../", "tmp");
 app.use(cors("*"))
